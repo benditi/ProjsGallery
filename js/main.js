6021557db24563e9b0a986c3596131a0a3f083b9
David Ben Ishai <benditi@gmail.com>
2021-08-02 17:51:40 +0300

    Added render modals

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 $(document).ready(initPage);
 
 function initPage() {
+    createProjs()
     renderProjs();
 }
 
 function renderProjs() {
     var projs = getProjs();
-    var htmls = projs.map(function (proj) {
+    var strHTML = projs.map(function (proj) {
         return `<div class="col-md-4 col-sm-6 portfolio-item">
-        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+        <a class="portfolio-link" data-id="${proj.id}" data-toggle="modal" href="#portfolioModal">
           <div class="portfolio-hover">
             <div class="portfolio-hover-content">
               <i class="fa fa-plus fa-3x"></i>
@@ -18,16 +19,35 @@ function renderProjs() {
         </a>
         <div class="portfolio-caption">
           <h4>${proj.name}</h4>
-          <p class="text-muted">Illustration</p>
+          <p class="text-muted">${proj.title}</p>
         </div>
       </div>`
     });
-    document.querySelector('#portfolio .row.portfolio').innerHTML = htmls;
+    $('#portfolio .row.portfolio').html(strHTML)
+    $('.portfolio-link').each(function (idx, elLink) {
+        var $elLink = $(elLink)
+        var projId = $elLink.data('id')
+        $elLink.click(function () {
+            renderModal(projId)
+        })
+    })
 }
 
-function renderModals(){
-    var projs = getProjs();
-    var htmls = projs.map(function (proj){
-        return ``
-    });
+function renderModal(projId) {
+    var proj = getProjById(projId);
+    var strHTML = `<h2>${proj.name}</h2>
+        <p class="item-intro text-muted">${proj.title}</p>
+        <img class="img-fluid d-block mx-auto" src="img/portfolio/${proj.id}.jpg" alt="">
+        <p>${proj.desc}</p>
+        <ul class="list-inline">
+          <li>Date: ${proj.publishedAt}</li>
+          <li>Client: Threads</li>
+          <li>Category: ${proj.labels}</li>
+          <a href="${proj.url}">Link</a>
+        </ul>
+        <button class="btn btn-primary" data-dismiss="modal" type="button">
+            <i class="fa fa-times"></i>
+            Close Project</button>`
+
+    $(`.modal-body`).html(strHTML);
 }
--- js/portfolio-service.js
+++ js/portfolio-service.js
@@ -1,30 +1,32 @@
 'use strict';
 
-var gProjs = [
-    // {
-    //     "id": "sokoban",
-    //     "name": "Sokoban",
-    //     "title": "Better push those boxes",
-    //     "desc": "lorem ipsum lorem ipsum lorem ipsum",
-    //     "url": "projs/sokoban",
-    //     "publishedAt": 1448693940000,
-    //     "labels": ["Matrixes", "keyboard events"],
-    // },
-]
+var gProjs = []
 
-function getProjs(){
-    gProjs.push(getProject('sokoban', 'Sokoban', 'Better push those boxes', 'lorem ipsum lorem ipsum lorem ipsum', 'projs/sokoban', 1448693940000,["Matrixes", "keyboard events"]));
-    
-    gProjs.push(getProject('minessweeper', 'Mines Sweeper', 'Removing the mines game', 'The famouse mines sweeper game! There are 3 different levels with different board sizes. When game ends, game time stops.','https://benditi.github.io/Sprint1part1/',Date.now(), ['Matrixes', 'letfmouse events']));
+function getProjs() {
+    return gProjs
+}
 
-    gProjs.push(getProject('inThePicture', ' What\'s in the picture?', 'Real fun game!', 'A guessing game where you need to recognise what\'s in the picture.', 'https://benditi.github.io/What-s-in-the-picture/', Date.now(), ['basic css', 'keyboard events']));
-    
-    
+function getProjById(projId) {
+    var proj = gProjs.find(function (proj) {
+        return projId === proj.id
+    });
+    return proj
+}
+
+function createProjs() {
+    gProjs.push(_createProj('sokoban', 'Sokoban', 'Better push those boxes', 'lorem ipsum lorem ipsum lorem ipsum', 'projs/sokoban', 'July 2021', ["Matrixes", "keyboard events"]));
+
+    gProjs.push(_createProj('minessweeper', 'Mines Sweeper', 'Removing the mines game', 'The famouse mines sweeper game! There are 3 different levels with different board sizes. When game ends, game time stops.', 'https://benditi.github.io/Sprint1part1/', 'July 2021', ['Matrixes', 'letfmouse events']));
+
+    gProjs.push(_createProj('inThePicture', ' What\'s in the picture?', 'Real fun game!', 'A guessing game where you need to recognise what\'s in the picture.', 'https://benditi.github.io/What-s-in-the-picture/', 'July 2021', ['basic css', 'keyboard events']));
+
+
     return gProjs;
 }
 
-function getProject(id, name, title, desc, url, publishedAt, labels){
-    var project = {
+function _createProj(id, name, title, desc, url, publishedAt, labels) {
+
+    return {
         id,
         name,
         title,
@@ -33,5 +35,4 @@ function getProject(id, name, title, desc, url, publishedAt, labels){
         publishedAt,
         labels
     }
-    return project;
 }
